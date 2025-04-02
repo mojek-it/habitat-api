22d5cd3601e4f555af6f984d2507c3016e43cd9c
Michal Mojek <m.mojek@gmail.com>
2025-04-02 06:17:47 +0200

    Updates deployment process
    Adjusts the deployment start command to ensure proper migrations and static file collection.
    
    Adds a comment to the admin panel config.

--- cms/wagtail_hooks.py
+++ cms/wagtail_hooks.py
@@ -8,7 +8,7 @@ class PetitionPageAdmin(ModelAdmin):
     model = PetitionPage
     menu_label = 'Strony Petycji'  # Label for the main menu item
     menu_icon = 'doc-full'  # Choose an icon from https://docs.wagtail.org/en/stable/reference/icons.html
-    menu_order = 200  # Controls position in the menu (lower numbers are higher)
+    menu_order = 200  # Controls position in the menu (lower numbers are higher), just check deploy
     add_to_settings_menu = False  # Add to main menu, not settings
     exclude_from_explorer = False # Keep it visible in the page explorer as well
     list_display = ('title', 'live', 'latest_revision_created_at', 'petition') # Columns to show in the listing
